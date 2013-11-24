22f82ad6dd8ae0eb9f7f864af55340311b74cd39
Paul Pamment <paul@byng-systems.com>
2013-11-24 18:47:40 +0000

    minor

--- website/views/scripts/default/artist.php
+++ website/views/scripts/default/artist.php
@@ -9,36 +9,52 @@ $artist = $this->artist;
         <hr/>
     </div>
     <div class="span4">
-        <h2><?php echo $artist->getName() ?></h2>
+        <h2><?php echo strtoupper($artist->getName()) ?></h2>
     </div>
     <div class="span4">
         <hr/>
     </div>
 </div>
 
-
-<ul class="thumbnails artist">
-    <?php foreach($artist->getChilds() as $artwork) : ?>
-    <li class="span3">
-        <a href="#img<?php echo $artwork->getId() ?>" data-toggle="lightbox">
-            <div class="thumbnail">
-                <div class="img-container">
-                    <img src="<?php echo $artwork->getImage()->getFullPath() ?>" />
-                </div>
-
-                <div class="caption">
-                    <h3><?php echo $artwork->getTitle() ?></h3>
-
-                    <p><?php echo $artwork->getDescription() ?></p>
-                </div>
-            </div>
-        </a>
-        <div id="img<?php echo $artwork->getId() ?>" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
-            <div class='lightbox-content'>
-                <img src="<?php echo $artwork->getImage()->getFullPath() ?>" />
-                <?php if($artwork->getCaption()) : ?><div class="lightbox-caption"><p><?php echo $artwork->getCaption() ?></p></div><?php endif; ?>
-            </div>
-        </div>
-    </li>
-    <?php endforeach; ?>
+<ul class="nav nav-pills pull-right">
+    <li class="active"><a href="#works" data-toggle="tab">WORKS</a></li>
+    <li><a href="#bio" data-toggle="tab">BIOGRAPHY</a></li>
 </ul>
+<div class="clearfix"></div>
+<div class="tab-content">
+    <div class="tab-pane active" id="works">
+        <ul class="thumbnails artist">
+            <?php foreach($artist->getChilds() as $artwork) : ?>
+                <li class="span3">
+                    <a href="#img<?php echo $artwork->getId() ?>" data-toggle="lightbox">
+                        <div class="thumbnail">
+                            <div class="img-container">
+                                <img src="<?php echo $artwork->getImage()->getFullPath() ?>" />
+                            </div>
+
+                            <div class="caption">
+                                <h3><?php echo $artwork->getTitle() ?></h3>
+
+                                <p><?php echo $artwork->getDescription() ?></p>
+                            </div>
+                        </div>
+                    </a>
+                    <div id="img<?php echo $artwork->getId() ?>" class="lightbox hide fade"  tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class='lightbox-content'>
+                            <img src="<?php echo $artwork->getImage()->getFullPath() ?>" />
+                            <?php if($artwork->getCaption()) : ?><div class="lightbox-caption"><p><?php echo $artwork->getCaption() ?></p></div><?php endif; ?>
+                        </div>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+    <div class="tab-pane" id="bio">
+        <div class="span6">
+            <img src="<?php echo $artist->getCover()->getImage()->getFullPath() ?>" />
+        </div>
+        <div class="span5">
+            <p><?php echo $artist->getBio() ?></p>
+        </div>
+    </div>
+</div>
